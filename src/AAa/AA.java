9ec579162c08a8789ef58a0d4c708e530b7f89f5
zaixiakanke <gjw102938@163.com>
2023-01-18 21:43:28 +0800

    ttttt1111

---
+++ src/AAa/AA.java
@@ -0,0 +1,18 @@
+package AAa;
+
+public class AA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("aaaaaaaa");
+	}
+	
+	public void say() {
+		System.out.println("aaa");
+	}
+	
+	public void say2() {
+		System.out.println("bbb");
+	}
+
+}
